a0ad3a09fce8af7f1de8b5829e6eaa0ed5313d75
AJ-SM <anujsidam1108@gmail.com>
2025-10-03 20:28:13 +0530

    added tailed

--- 3d-view/app/scene/page.tsx
+++ 3d-view/app/scene/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
 export default function scene(){
@@ -12,7 +13,7 @@ export default function scene(){
             const scene = new THREE.Scene();
             const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
             const renderer = new THREE.WebGLRenderer();
-            renderer.setSize(window.innerWidth,window.innerHeight);
+            renderer.setSize(window.innerWidth/2,window.innerHeight/2);
             cret.current?.appendChild(renderer.domElement)
             camera.position.z=5;
             
@@ -20,13 +21,25 @@ export default function scene(){
 
 
             if (typeof window !== 'undefined'){
-            const geom = new THREE.BoxGeometry();
+            const geom = new THREE.BoxGeometry(1,1,1);
             const mat = new THREE.MeshBasicMaterial({color:0x00ff00})
             const cub  = new THREE.Mesh(geom,mat);
             scene.add(cub)
             renderer.render(scene,camera)
+
+            const animate = ()=>{
+            cub.rotation.x += 0.01;
+            cub.rotation.y += 0.01;
+            renderer.render(scene, camera);
+            requestAnimationFrame(animate);
+
+            }
+            animate()
+
+
         }
         },[])
+
 
 
 
@@ -35,5 +48,12 @@ export default function scene(){
         return <div ref={cret}/>
 
     }
-    return (<ThreeScene/>)
+    return (
+    
+    <>
+    <div className='flex justify-center items-center h-screen w-screen'>
+        <ThreeScene  />
+    </div>
+    
+    </>)
 }
